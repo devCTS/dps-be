53997dd88883052fd9e2127c138837c66a191d01
devCTS <dev@catalyst.sh>
2024-09-10 20:00:15 +0530

    core: merchant and member apis

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import { ValidationExceptionFilter } from './utils/exception-filters/errorMap';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['*'],
-    credentials: true,
-  });
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
