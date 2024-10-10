801390df3080a022bfdce7d5c97d1911b6a03aa3
devCTS <dev@catalyst.sh>
2024-10-10 15:22:29 +0530

    Added update status in payout service

--- src/payout/payout.service.ts
+++ src/payout/payout.service.ts
@@ -1,9 +1,10 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePayoutDto } from './dto/create-payout.dto';
 import { UpdatePayoutDto } from './dto/update-payout.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Payout } from './entities/payout.entity';
 import { Repository } from 'typeorm';
+import { OrderStatus } from 'src/utils/enum/enum';
 
 @Injectable()
 export class PayoutService {
@@ -15,6 +16,16 @@ export class PayoutService {
   async create(createPayoutDto: CreatePayoutDto) {
     const payout = await this.payoutRepository.save({ ...createPayoutDto });
     return HttpStatus.CREATED;
+  }
+
+  async updatePayoutStatus(id: number, status: OrderStatus) {
+    const payinOrderDetails = await this.payoutRepository.findOneBy({ id });
+
+    if (!payinOrderDetails) throw new NotFoundException('Order not found');
+
+    await this.payoutRepository.update(id, { status });
+
+    return HttpStatus.OK;
   }
 
   findAll() {
