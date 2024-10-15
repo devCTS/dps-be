c6b8e6e0f4545621be371e321140119346b220b9
devCTS <dev@catalyst.sh>
2024-10-15 16:52:23 +0530

    fixed expose of secret keys

--- src/gateway/dto/phonepe-response.dto.ts
+++ src/gateway/dto/phonepe-response.dto.ts
@@ -1,13 +1,18 @@
+import { Exclude, Expose } from 'class-transformer';
 import { IsBoolean, IsNotEmpty, IsNumber } from 'class-validator';
 
+@Exclude()
 export class PhonepeResponseDto {
+  @Expose()
   @IsNumber()
   id: number;
 
+  @Expose()
   @IsBoolean()
   @IsNotEmpty()
   incoming: boolean;
 
+  @Expose()
   @IsBoolean()
   @IsNotEmpty()
   outgoing: boolean;
