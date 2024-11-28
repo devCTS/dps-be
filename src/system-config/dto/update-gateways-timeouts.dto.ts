89650d0f5242cb44dbc034e242f9f067cd288b6f
devCTS <dev@catalyst.sh>
2024-11-28 15:50:12 +0530

    added object validator in DTO

--- src/system-config/dto/update-gateways-timeouts.dto.ts
+++ src/system-config/dto/update-gateways-timeouts.dto.ts
@@ -1,13 +1,17 @@
-import { IsNumber, IsObject } from 'class-validator';
+import { IsNumber, IsObject, Validate } from 'class-validator';
+import { GatewayName } from 'src/utils/enum/enum';
 
 export class UpdateGatewaysTimeoutsDto {
   @IsObject()
+  @GatewayValidator()
   defaultPayinGateway: object;
 
   @IsObject()
+  @GatewayValidator()
   defaultPayoutGateway: object;
 
   @IsObject()
+  @GatewayValidator()
   defaultWithdrawalGateway: object;
 
   @IsNumber()
@@ -15,4 +19,17 @@ export class UpdateGatewaysTimeoutsDto {
 
   @IsNumber()
   payoutTimeout: number;
+}
+
+function GatewayValidator() {
+  return Validate((value: object) => {
+    if (typeof value !== 'object' || Array.isArray(value)) return false;
+
+    for (const key in value) {
+      const numberKey = parseInt(key, 10);
+      if (isNaN(numberKey) || !Object.values(GatewayName).includes(value[key]))
+        return false;
+    }
+    return true;
+  });
 }
