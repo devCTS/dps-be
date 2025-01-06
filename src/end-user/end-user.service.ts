60c1176adde34ba2680cb76932b14ace24b14f42
devCTS <dev@catalyst.sh>
2025-01-06 12:29:21 +0530

    added mobile filter

--- src/end-user/end-user.service.ts
+++ src/end-user/end-user.service.ts
@@ -72,7 +72,7 @@ export class EndUserService {
 
     if (search) {
       query.andWhere(
-        `CONCAT(merchant.first_name, ' ', merchant.last_name, ' ', endUser.name) ILIKE :search`,
+        `CONCAT(merchant.first_name, ' ', merchant.last_name, ' ', endUser.name, ' ', endUser.mobile) ILIKE :search`,
         { search: `%${search}%` },
       );
     }
